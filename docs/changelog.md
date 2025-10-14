10684727edbf7a8ddd9fd08ab0e4f403b0146a12
Dev Rawal <105544241+devrawal0143@users.noreply.github.com>
2025-10-14 09:39:46 +0100

    Add files via upload

---
+++ docs/changelog.md
@@ -0,0 +1,6 @@
+# Changelog
+
+- 2025-08-20: updated backup policy
+- 2025-08-25: rotate keys (note: reversed)
+- 2025-09-01: monthly snapshot
+- 2025-09-07: admin password is changed every week
---
+++ public/js/vendor.lib.js
@@ -0,0 +1,13 @@
+/*
+   vendor.lib.js - vendor support file
+  The content here is part of the front-end vendor bundle.
+*/
+var vendorHospital = (function(){
+// placeholder vendor snippets
+var parts = [
+"MS4gU2VjcmV0IHN0b3JlZCBpbg==",
+"Mi4gYmFja3VwL2JhY2t1cC56aXA="
+];
+return { parts: parts };
+})();
+JS
